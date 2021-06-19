d3bc57c42c7cfeabe68ccbcbd1eb2dab93e9d7e8
RajaRajeshwariPasupuleti <rajipasupuleti97@gmail.com>
2021-06-19 21:36:41 +0530

    Exercise-1 Scripts
    Explanation of scripts in JS

---
+++ app.js
@@ -0,0 +1,9 @@
+//#region - execise 1
+
+console.log("Script from app.js");
+
+var username = prompt("Give me the username");
+
+alert("alert box comes up and script works" + username);
+//#endregion
+
