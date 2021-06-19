ab178ac5b314ff95c19191a18a62a4bcda1dd93b
RajaRajeshwariPasupuleti <rajipasupuleti97@gmail.com>
2021-06-19 22:12:19 +0530

    ButtonClick  and Text Area
    Contains details of  ex-3,4.
    Excluded scripts console logs for time being in execution

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 //#region - execise 1
 
-console.log("Script from app.js");
+//  console.log("Script from app.js");
 
-var username = prompt("Give me the username");
+// var username = prompt("Give me the username");
 
-alert("alert box comes up and script works" + username);
+// alert("alert box comes up and script works" + username);
 //#endregion
+
+
+// reference to btn or other input tags in javascript
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-Input");
+
+//callback function
+function clickEventHandler()
+{
+    console.log("clicked");
+    console.log("input value", txtInput.value);
+}
+
+//event listener
+btnTranslate.addEventListener("click",  clickEventHandler)
 
