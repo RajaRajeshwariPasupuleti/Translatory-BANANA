e48270c6d75e831975c60642a9ab18ac7ebab5be
RajaRajeshwariPasupuleti <rajipasupuleti97@gmail.com>
2021-06-20 14:06:43 +0530

    Translator - Completed
    A complete banana translator app

--- app.js
+++ app.js
@@ -13,11 +13,47 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-Input");
 var outputDiv = document.querySelector("#output");
 
+
+
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+
+
+
+function getTranslationURL(text){
+    return url + "?" + "text=" + text
+}
+
+
+function catchError(error){
+    console.log("error Occured " + error)
+    // alert("Something went wrong with server, Please try again");
+}
+
+
 //callback function
 function clickEventHandler()
 {
-    outputDiv.innerText = "some appending text" + txtInput.value;
+    // outputDiv.innerText = "some appending text" + txtInput.value;
+
+
+    var inputTxt = txtInput.value;
+
+    //calling server for processing
+    fetch(getTranslationURL(inputTxt))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText; // output
+        console.log(json.contents.translated);
+    })
+     .catch(error => console.log(error))
+    //.catch(catchError)   -> 2 way through function
+
 }
+
+
+
 
 //event listener
 btnTranslate.addEventListener("click",  clickEventHandler)
